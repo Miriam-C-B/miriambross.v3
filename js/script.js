782dc54df7136e1b480b8ea9d76a947301fe5c0f
miriam_b <MiriamBross27@gmail.com>
2022-03-17 17:29:02 +0100

    Added code to js file to enable bootstrap tool tips

---
+++ js/script.js
@@ -0,0 +1,6 @@
+//lines of code that enable tool tips in the document if needed
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+//end of lines for tool tips
